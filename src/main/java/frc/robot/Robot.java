3193e3355b81019c158b213a3522a5ff1571fc3a
Hannahjjj97 <hannahjjj97@gmail.com>
2025-08-24 23:00:47 +0800

    fix: add missing break statement in autonomous mode switch case

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -179,6 +179,7 @@ public class Robot extends TimedRobot {
         break;
       case middleOneCoralAndBackRight:
         middleOneCoralAndBackRight();
+        break;
       default:
         System.out.println("Unknown auto selected: " + autoSelected);
         break;
